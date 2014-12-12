fb650954ea5c358d7c02bd33a130642d49f01b9d
guneysus <serefguneysu@gmail.com>
2014-12-13 00:22:44 +0200

    Api redesigned insteaf of distutils setuptools used Although dependencies are defined in setup.py added an extra reqs.txt tests organized and can be run by nosetest easily or run the tests.py file

--- tests.py
+++ tests.py
@@ -1,72 +1,74 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from unicode_tr import unicode_tr
+import unicode_tr
 from unicode_tr.extras import slugify
 
-import unittest
 
 
-class TestTurkishWords(unittest.TestCase):
+UPPER_CASES = [
+    {"word": u"ığdır", "upper": u"IĞDIR"},
+    {"word": u"ırmak", "upper": u"IRMAK"},
+    {"word": u"timu", "upper": u"TİMU",}
+]
 
-    UPPER_CASES = [
-        {"word": u"ığdır", "upper": u"IĞDIR"},
-        {"word": u"ırmak", "upper": u"IRMAK"},
-        {"word": u"timu", "upper": u"TİMU",}
-    ]
+LOWER_CASES = [
+    {"word": u"İstanbul", "lower": u"istanbul"},
+    {"word": u"Irmak", "lower": u"ırmak"},
+    {"word": u"ÇESİL", "lower": u"çesil"},
+    {"word": u"AĞA", "lower": u"ağa"},
+]
 
-    LOWER_CASES = [
-        {"word": u"İstanbul", "lower": u"istanbul"},
-        {"word": u"Irmak", "lower": u"ırmak"},
-        {"word": u"ÇESİL", "lower": u"çesil"},
-        {"word": u"Ğaaaa", "lower": u"ğaaaa"},
-    ]
+CAPITALIZE_CASES = [
+    {"word": u"KADIKÖY", "capitalize": u"Kadıköy"},
+    {"word": u"çınar", "capitalize": u"Çınar"},
+    {"word": u"şansal", "capitalize": u"Şansal"},
+    {"word": u"istanbul", "capitalize": u"İstanbul",}
+]
 
-    CAPITALIZE_CASES = [
-        {"word": u"KADIKÖY", "capitalize": u"Kadıköy"},
-        {"word": u"çınar", "capitalize": u"Çınar"},
-        {"word": u"şansal", "capitalize": u"Şansal"},
-        {"word": u"istanbul", "capitalize": u"İstanbul",}
-    ]
+TITLE_CASES = [
+    {"phrase": u"ısparta", "title": u"Isparta"},
+    {"phrase": u"ısparta istanbul", "title": u"Isparta İstanbul"},
+    {"phrase": u"İstanbul", "title": u"İstanbul"},
+    {"phrase": u"çarşı timu", "title": u"Çarşı Timu"},
+    {"phrase": u"aĞa ÇEŞİL KADIKÖY", "title": u"Ağa Çeşil Kadıköy"},
+    {"phrase": u"ŞamaTa ısparta istanbul", "title": u"Şamata Isparta İstanbul"},
+]
 
-    TITLE_CASES = [
-        {"phrase": u"ısparta", "title": u"Isparta"},
-        {"phrase": u"ısparta istanbul", "title": u"Isparta İstanbul"},
-        {"phrase": u"İstanbul", "title": u"İstanbul"},
-        {"phrase": u"çarşı timu", "title": u"Çarşı Timu"},
-        {"phrase": u"Ğaaa ÇEŞİL KADIKÖY", "title": u"Ğaaa Çeşil Kadıköy"},
-        {"phrase": u"ŞamaTa ısparta istanbul", "title": u"Şamata Isparta İstanbul"},
-    ]
+SLUG_CASES = [
+    {"phrase": "Türkçe", "slug": "turkce"},
+    {"phrase": "Diyarbakır", "slug": "diyarbakir"},
+    {"phrase": "Yeni başlayanlar için yalnızlık", "slug": "yeni-baslayanlar-icin-yalnizlik"},
+]
 
-    SLUG_CASES = [
-        {"phrase": "Türkçe", "slug": "turkce"},
-        {"phrase": "Diyarbakır", "slug": "diyarbakir"},
-        {"phrase": "Yeni başlayanlar için yalnızlık", "slug": "yeni-baslayanlar-icin-yalnizlik"},
-    ]
 
-    def test_upper(self):
-        for case in self.UPPER_CASES:
-            word = unicode_tr(case.get("word"))
-            self.assertEquals(word.upper(), case.get("upper"))
+def test_upper():
+    for case in UPPER_CASES:
+        word = case.get("word")
+        assert(word.upper() == case.get("upper"))
 
-    def test_lower(self):
-        for case in self.LOWER_CASES:
-            word = unicode_tr(case.get("word"))
-            self.assertEquals(word.lower(), case.get("lower"))
+def test_lower():
+    for case in LOWER_CASES:
+        word = case.get("word")
+        assert(word.lower() == case.get("lower") )
 
-    def test_capitalize(self):
-        for case in self.CAPITALIZE_CASES:
-            word = unicode_tr(case.get("word"))
-            self.assertEquals(word.capitalize(), case.get("capitalize"))
+def test_capitalize():
+    for case in CAPITALIZE_CASES:
+        word = case.get("word")
+        assert(word.capitalize() ==  case.get("capitalize"))
 
-    def test_title(self):
-        for case in self.TITLE_CASES:
-            phrase = unicode_tr(case.get("phrase"))
-            self.assertEquals(phrase.title(), case.get("title"))
+def test_title():
+    for case in TITLE_CASES:
+        phrase = case.get("phrase")
+        assert(phrase.title() == case.get("title"))
 
-    def test_slugify(self):
-        for case in self.SLUG_CASES:
-            self.assertEquals(slugify(case.get("phrase")), case.get("slug"))
+def test_slugify():
+    for case in SLUG_CASES:
+        assert(slugify(case.get("phrase")) == case.get("slug"))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    test_upper()
+    test_lower()
+    test_capitalize()
+    test_title()
+    test_slugify()
--- unicode_tr/__init__.py
+++ unicode_tr/__init__.py
@@ -1,33 +1,49 @@
 # -*- coding: utf8 -*-
+# Redesigned by @guneysus
 
+import __builtin__
+from forbiddenfruit import curse
 
-class unicode_tr(unicode):
-    CHARMAP = {
-        "to_upper": {
-            u"ı": u"I",
-            u"i": u"İ",
-        },
-        "to_lower": {
-            u"I": u"ı",
-            u"İ": u"i",
-        }
-    }
+lcase_table = tuple(u'abcçdefgğhıijklmnoöprsştuüvyz')
+ucase_table = tuple(u'ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ')
 
-    def lower(self):
-        for key, value in self.CHARMAP.get("to_lower").items():
-            self = self.replace(key, value)
+def upper(data):
+    data = data.replace('i',u'İ')
+    data = data.replace(u'ı',u'I')
+    result = ''
+    for char in data:
+        try:
+            char_index = lcase_table.index(char)
+            ucase_char = ucase_table[char_index]
+        except:
+            ucase_char = char
+        result += ucase_char
+    return result
 
-        return self.lower()
+def lower(data):
+    data = data.replace(u'İ',u'i')
+    data = data.replace(u'I',u'ı')
+    result = ''
+    for char in data:
+        try:
+            char_index = ucase_table.index(char)
+            lcase_char = lcase_table[char_index]
+        except:
+            lcase_char = char
+        result += lcase_char
+    return result
 
-    def upper(self):
-        for key, value in self.CHARMAP.get("to_upper").items():
-            self = self.replace(key, value)
+def capitalize(data):
+    return data[0].upper() + data[1:].lower()
 
-        return self.upper()
+def title(data):
+    return " ".join(map(lambda x: x.capitalize(), data.split()))
 
-    def capitalize(self):
-        first, rest = self[0], self[1:]
-        return unicode_tr(first).upper() + unicode_tr(rest).lower()
+curse(__builtin__.unicode, 'upper', upper)
+curse(__builtin__.unicode, 'lower', lower)
+curse(__builtin__.unicode, 'capitalize', capitalize)
+curse(__builtin__.unicode, 'title', title)
 
-    def title(self):
-        return " ".join(map(lambda x: unicode_tr(x).capitalize(), self.split()))
+if __name__ == '__main__':
+    print u'istanbul'.upper()
+    print u'İSTANBUL'.lower()
